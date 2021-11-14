1b4c1c43a21133f9671183e438bcb28b314c4395
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-14 18:46:27 +0530

    added print syntax

--- 02-print.sh
+++ 02-print.sh
@@ -10,3 +10,10 @@
 echo Hello World
 echo Welcome
 
+# ESC sequences, \n (new line), \e (new tab)
+
+# syntax : echo -e "message\nnew line"
+# To enable any esc seq we need to enable -e option.
+# Also the input should be in quotes, preferably double quotes.
+
+echo -e "Hello World\nWelcome"
