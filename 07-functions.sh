89f2c367bdfa21ca275812806907c29f414ea3d1
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-14 22:01:45 +0530

    added functions

---
+++ 07-functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## functions should be always declared before using it, same like variables.
+## so that is the reason, Function we always find in starting of the scripts.
+
+function abc () {
+  echo iam a function abc
+  }
+
+##main program
+
+abc
+
