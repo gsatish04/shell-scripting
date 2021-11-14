0948639501ca918f36e8b4bf94b75d1bc4a4c1c6
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-14 18:38:41 +0530

    added she bang

---
+++ 02-print.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+## To print some text on screen then we can use echo command or printf command
+## we choose to go with echo command because of its less syntaxing.
+
+#syntax
+# echo message to print
+
+echo Hello World
+echo Welcome
+
