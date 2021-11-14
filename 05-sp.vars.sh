04f25e112e71b2084835cf76d58c74ed6174df12
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-14 20:44:05 +0530

    added inputs before execution

--- 05-sp.vars.sh
+++ 05-sp.vars.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+#0 is script name
+
 echo 0 = $0
 
+#First argument to script
 echo 1 = $1
+#second argument to script
 echo 2 = $2
 
+## * and @ are giving all the arguments
 echo "* = $*"
 echo "@ = $@"
+
+# number of values which are parsed
 echo "# = $#"
+
+echo -e "your name = $1\nyour age = $2"
 
 
 
