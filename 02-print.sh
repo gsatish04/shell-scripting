54ef85a724d1645edd0f7ff975858957e5b3aec2
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-14 18:58:45 +0530

    added print color syntax

--- 02-print.sh
+++ 02-print.sh
@@ -17,3 +17,25 @@ echo Welcome
 # Also the input should be in quotes, preferably double quotes.
 
 echo -e "Hello World\nWelcome"
+
+echo -e "word1\t\tword2"
+
+# Colored Output
+# Syntax: echo -e "\e[COLmMessage"
+
+## Colors     Code
+# Red       31
+# Green     32
+# Yellow    33
+# Blue      34
+# Magenta   35
+# Cyan      36
+
+echo -e "\e[31mText in Red Color"
+echo -e "\e[32mText in Green Color"
+echo -e "\e[33mText in Yellow Color"
+echo -e "\e[34mText in Blue Color"
+echo -e "\e[35mText in Magenta Color"
+echo -e "\e[36mText in Cyan Color"
+
+
