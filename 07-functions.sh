a35aad0132a37178cdb12e8cb8eb8b1374306cc7
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-14 22:22:14 +0530

    added functions

--- 07-functions.sh
+++ 07-functions.sh
@@ -8,6 +8,7 @@ function abc () {
   a=100
   echo a in function = $a
   b=20
+  return 30
   echo First Argument in Function = $1
   }
 
@@ -21,7 +22,9 @@ xyz () {
 ##main program
 
 a=10
-abc Sati
+#abc Sati
+abc $1
+echo exit status of abc = $?
 echo b in main program =$b
 xyz
 echo First Argument in Main Program = $1
