e0f22f584c15fa625c27a9ff2236f42907bbd2eb
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-15 21:59:10 +0530

    modified echo and print

---
+++ roboshop/components/frontend.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+Print() {
+  echo -e "\e[1m$1\e[0m"
+}
+echo -e "\e[1mInstalling Nginx\e[0m"
+yum install nginx -y
+
+echo -e "\e[1mEnabling Nginx\e[0m"
+systemctl enable nginx
+echo -e "\e[1mStarting Nginx\e[0m"
+systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
