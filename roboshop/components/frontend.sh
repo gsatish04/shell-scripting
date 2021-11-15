ca534ee3be14fd3dcaea894447c159b2e76ae048
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-15 22:44:23 +0530

    added if uid

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -11,6 +11,13 @@ rm -f $LOG
 Print "Installing Nginx"
 yum install nginx -y &>>$LOG
 
+if [ $? -eq 0 ] ; then
+  echo -e "\e[1;32mSUCCESS\e[0m"
+else
+    echo -e "\e[1;31mfailure\e[0m"
+
+fi
+
 Print "Enabling Nginx"
 systemctl enable nginx
 Print "Starting Nginx"
