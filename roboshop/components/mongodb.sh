73c488a2d75875dd487b298638cf5f5728ce66fd
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-15 23:20:17 +0530

    created common file

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,6 @@
 source components/common.sh
 
+
+Print "Download Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https: //raw.githubuserconsent.com/roboshop-devops-project/mongodb/main/mongo.repo
+Stat $?
