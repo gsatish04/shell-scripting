b6e59e8647711efa5bb720f57d936ba1d523617f
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-15 22:34:54 +0530

    if statement

--- 08-if.sh
+++ 08-if.sh
@@ -2,7 +2,7 @@
 
 read -p 'Enter username: ' username
 
-if [ "$username" =="root"]; then
+if [ "$username" == "root" ]; then
   echo "Hey , you are root user"
   else
     echo Hey, you are nonroot user
