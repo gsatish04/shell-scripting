a2d4de543fb6efca0e3703c596a0a695f15f0e8a
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-15 22:01:33 +0530

    modified echo with print command

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,12 +3,12 @@
 Print() {
   echo -e "\e[1m$1\e[0m"
 }
-echo -e "\e[1mInstalling Nginx\e[0m"
+Print "Installing Nginx"
 yum install nginx -y
 
-echo -e "\e[1mEnabling Nginx\e[0m"
+Print "Enabling Nginx"
 systemctl enable nginx
-echo -e "\e[1mStarting Nginx\e[0m"
+Print "Starting Nginx"
 systemctl start nginx
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
