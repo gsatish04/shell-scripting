40f8dc1d2d14d66a83e78f9e28d905277b29f785
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-16 23:09:46 +0530

    cart instance

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -5,7 +5,7 @@ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Re
 
 
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$1}" | jq
+  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro sg-053f6428e2a50d485 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$1}" | jq
 else
   echo "Instance already exit"
 fi
