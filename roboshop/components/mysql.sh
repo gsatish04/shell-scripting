5d92d60c97623c8c57b4698b3f86dfdb08415d32
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 18:31:39 +0530

    modified at add roboshop user

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -11,7 +11,6 @@ Print "Setup MySQL Repo"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG
 Stat $?
 
-exit
 Print "Install MariaDB Service"
 yum remove mariadb-libs -y &>>$LOG && yum install mysql-community-server -y &>>$LOG
 Stat $?
