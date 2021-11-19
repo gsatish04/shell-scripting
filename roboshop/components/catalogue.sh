7e4546cc38ae9d0801411dd9353bb3d94f353299
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 11:33:18 +0530

    modified at add roboshop user

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,28 +3,28 @@
 source components/common.sh
 
 Print "Install NodeJS"
-yum install nodejs make gcc-c++ -y &>>LOG
+yum install nodejs make gcc-c++ -y &>>$LOG
 Stat $?
 
 Print "Add Roboshop User"
-id roboshop &>>LOG
+id roboshop &>>$LOG
 if  [ $? -eq 0 ]; then
   echo User Roboshop already exists
 else
-  useradd roboshop &>>LOG
+  useradd roboshop &>>$LOG
 fi
 Stat $?
 
 Print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 Stat $?
 
 Print "Remove Old Content"
-rm -rf /home/roboshop/catalogue &>>LOG
+rm -rf /home/roboshop/catalogue &>>$LOG
 Stat $?
 
 Print "Extract Catalogue"
-unzip -o -d /home/roboshop /tmp/catalogue.zip &>>LOG
+unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
 Stat $?
 
 Print "Copy Content"
@@ -33,18 +33,29 @@ Stat $?
 
 Print "Install NodesJS dependencies"
 cd /home/roboshop/catalogue
-npm install &>>LOG
+npm install &>>$LOG
 Stat $?
 
 Print " Fix App Permission"
 crown -R roboshop:roboshop /home/roboshop
 Stat $?
 
+Print "Update DNS records in SystemD config"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG
+Stat $?
 
-exit
+Print "Copy SystemD file"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG
+Stat $?
 
+Print "Start Catalogue Service"
+systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
+Stat $?
 
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable catalogue
+Print "Checking DB Connections from APP"
+STAT=$(curl -s localhost:8080/health | jq .mongo)
+if [ $STAT == "true"] then
+  Stat 0
+else
+    Stat 1
+fi
