42848fa5c93df9a7402c96959dbc81e8d44d95c3
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 12:34:25 +0530

    modified at add roboshop user

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,5 +1,14 @@
 Print() {
-  echo -n -e "\e[1m$1\e[0m ..."
+
+  LSPACE=$(echo $1 | awk '{print length}')
+  SPACE=$((MSPACE-$LSPACE))
+  SPACES=""
+  while [$SPACE -gt 0]; do
+    SPACES="$SPACES$(echo ' ')"
+    SPACE=$(($SPACE-1))
+  done
+
+  echo -n -e "\e[1m$1${SPACEs}\e[0m ..."
   echo -e "\n\e[36m==================$1================\e[0m" >>$LOG
 }
 
