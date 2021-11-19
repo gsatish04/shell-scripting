f1e1dd8563f0723c9254b9d1453e3c623afe58bd
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 17:09:12 +0530

    modified at add roboshop user

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -77,10 +77,10 @@ NODEJS() {
 
 }
 
-CHECK_MONGO_FROM_APP(){
+CHECK_REDIS_FROM_APP(){
   Print "Checking DB Connections from APP"
   sleep 5
-  STAT=$(curl -s localhost:8080/health | jq .mongo)
+  STAT=$(curl -s localhost:8080/health | jq .redis)
   if [ "$STAT" == "true" ]; then
   Stat 0
   else
