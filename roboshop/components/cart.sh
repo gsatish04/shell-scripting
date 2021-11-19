b3e7645a0b218b7fe2829d7d4d7a2349d8b7dfe4
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 16:14:43 +0530

    modified at add roboshop user

---
+++ roboshop/components/cart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source components/common.sh
+
+MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+
+COMPONENT_NAME=Cart
+COMPONENT=cart
+NODEJS
+
+CHECK_MONGO_FROM_APP
