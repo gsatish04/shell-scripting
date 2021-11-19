ae89382f03164bb8de14e5d983ba44777f98e950
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 15:22:27 +0530

    modified at add roboshop user

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,60 +4,12 @@ source components/common.sh
 
 MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-
-Print "Install NodeJS"
-yum install nodejs make gcc-c++ -y &>>$LOG
-Stat $?
-
-Print "Add Roboshop User"
-id roboshop &>>$LOG
-if  [ $? -eq 0 ]; then
-  echo User Roboshop already exists &>>$LOG
-else
-  useradd roboshop &>>$LOG
-fi
-Stat $?
-
-Print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
-Stat $?
-
-Print "Remove Old Content"
-rm -rf /home/roboshop/catalogue &>>$LOG
-Stat $?
-
-Print "Extract Catalogue"
-unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
-Stat $?
-
-Print "Copy Content"
-mv /home/roboshop/catalogue-main /home/roboshop/catalogue
-Stat $?
-
-Print "Install NodesJS dependencies"
-cd /home/roboshop/catalogue
-npm install --unsafe-perm &>>$LOG
-Stat $?
-
-Print "Fix App Permissions"
-chown -R roboshop:roboshop /home/roboshop
-Stat $?
-
-Print "Update DNS records in SystemD config"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG
-Stat $?
-
-Print "Copy SystemD file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG
-Stat $?
-
-Print "Start Catalogue Service"
-systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
-Stat $?
-
-sleep 5
+COMPONENT_NAME=Catalogue
+COMPONENT=catalogue
+NODEJS
 
 Print "Checking DB Connections from APP"
+sleep 5
 STAT=$(curl -s localhost:8080/health | jq .mongo)
 echo Status = $STAT
 if [ "$STAT" == "true" ]; then
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -12,13 +12,69 @@ Print() {
 }
 
 Stat(){
-if [ $? -eq 0 ] ; then
+ if [ $? -eq 0 ] ; then
   echo -e "\e[1;32mSUCCESS\e[0m"
-else
+ else
     echo -e "\e[1;31mfailure\e[0m"
     echo -e "\e[1;33m Script Failed and check the detailed log in $LOG file\e[0m"
-fi
+    exit 1
+ fi
 }
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+NODEJS() {
+  Print "Install NodeJS"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  Stat $?
+
+  Print "Add Roboshop User"
+  id roboshop &>>$LOG
+  if  [ $? -eq 0 ]; then
+    echo User Roboshop already exists &>>$LOG
+  else
+    useradd roboshop &>>$LOG
+  fi
+  Stat $?
+
+  Print "Download $COMPONENT_NAME"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  Stat $?
+
+  Print "Remove Old Content"
+  rm -rf /home/roboshop/${COMPONENT} &>>$LOG
+  Stat $?
+
+  Print "Extract ${COMPONENT} Content"
+  unzip -o -d /home/roboshop /tmp/${COMPONENT}.zip &>>$LOG
+  Stat $?
+
+  Print "Copy Content"
+  mv /home/roboshop/user-main /home/roboshop/${COMPONENT}
+  Stat $?
+
+  Print "Install NodesJS dependencies"
+  cd /home/roboshop/${COMPONENT}
+  npm install --unsafe-perm &>>$LOG
+  Stat $?
+
+  Print "Fix App Permissions"
+  chown -R roboshop:roboshop /home/roboshop
+  Stat $?
+
+  Print "Update DNS records in SystemD config"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal '/home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  Stat $?
+
+  Print "Copy SystemD file"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG
+  Stat $?
+
+  Print "Start ${COMPONENT_NAME} Service"
+  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  Stat $?
+
+
+
+}
