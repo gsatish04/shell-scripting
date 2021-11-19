e4bfe2c8279defff4c063efab5e989a264065a6d
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-19 10:54:10 +0530

    modified at add roboshop user

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -17,3 +17,6 @@ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Re
 
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${p}/" record.json >/tmp/record.json
 
+aws route53 change-resource-record-sets  --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq
+
+
