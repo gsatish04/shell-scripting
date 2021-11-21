8b37e144fb2f8f27fb2416b9fe05013e4daf84f5
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-21 11:27:20 +0530

    for loop added

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
+CREATE() {
+  COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 
-if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids  sg-053f6428e2a50d485 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$1}" | jq
+ if [ $COUNT -eq 0 ]; then
+  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids  sg-053f6428e2a50d485 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$1}" | jq &>/dev/null
+ else
+  echo -e "\e[1;33m$1 Instance already exists\e[0m"
+  return
+ fi
+
+ sleep 5
+ IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+ ## xargs is used to remove the double quotes
+}
+
+if [ "$1" == "all"]; then
+  ALL=(frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment)
+  for component in ${ALL[*]}; do
+    echo "Creating Instance - $component"
+    CREATE $component
+  done
 else
-  echo "Instance already exists"
+    CREATE $1
 fi
-
-sleep 5
-IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
-## xargs is used to remove the double quotes
-
-
-sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-
-aws route53 change-resource-record-sets  --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq
-
 
