e5f747bb4a5fff9865336b9ef65e1f0912382376
Satish Kumar <gsatishkumar04@gmail.com>
2021-11-21 10:30:06 +0530

    modified at add roboshop user

---
+++ 08-loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Loops are mainly two major commands- while and for.
+# while loop works on the Expressions that we used in If statements.
+# while loop- all the time its control the expressions.
+
+a=10
+while [ $a -gt 0 ]; do
+  echo while loop
+  sleep 0.5
+  a=$(($a-1))
+done
+
+# above a=10 is it repeats ten times. and double braces (()) means arithamatic expression.
